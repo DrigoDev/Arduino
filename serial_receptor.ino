1c476a4e0af461d8deaf4f98c5dafe550fb72e57
Rodrigo Selistre <drigonba@gmail.com>
2017-08-10 16:46:45 -0300

    Add files via upload

---
+++ serial_receptor.ino
@@ -0,0 +1,18 @@
+
+int l = 13;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(l, OUTPUT);
+}
+
+void loop(){
+  if(Serial.available()){
+  int v = Serial.read();
+  Serial.println(v);
+      digitalWrite(l, HIGH);
+  delay(1000);
+  digitalWrite(l, LOW);
+  delay(1000);
+    }
+  }
